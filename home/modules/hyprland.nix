0156386f88f5c78966bb3ab8482227a3a71791cd
Quandrax <128057564+Quandrax@users.noreply.github.com>
2025-09-05 18:57:04 +0200

    Update hyprland.nix

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -73,12 +73,37 @@
         "$mod, 7, workspace, 7"
         "$mod, 8, workspace, 8"
         "$mod, 9, workspace, 9"
-        "$mod, 0, workspace, 10"     
+        "$mod, 0, workspace, 10"
+
+        "$mod, left, swapwindow, l"
+        "$mod, right, swapwindow, r"
+        "$mod, up, swapwindow, u"
+        "$mod, down, swapwindow, d"
+
+        "SUPER_SHIFT, left, movefocus, l"
+        "SUPER_SHIFT, right, movefocus, r"
+        "SUPER_SHIFT, up, movefocus, u"
+        "SUPER_SHIFT, down, movefocus, d"
+
+        "$mod, mouse_up, workspace, e-1"
+        "$mod, mouse_down, workspace, e+1"
+
+        "SUPER_SHIFT, 1, movetoworkspacesilent, -1"
+        "SUPER_SHIFT, 2, movetoworkspacesilent, +1"     
       ];
 
       bindm = [
         "$mod, mouse:272, movewindow"
         "$mod, mouse:273, resizewindow"
+      ];
+
+      bindel = [
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 1%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
       ];
     };
   };
