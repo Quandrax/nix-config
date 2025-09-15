080e1ae0782765d077e6efa434cc530f8e82a937
Quandrax <128057564+Quandrax@users.noreply.github.com>
2025-09-15 16:15:54 +0200

    Update hardware-configuration.nix

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -36,5 +36,5 @@
   # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirm>;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirm;
 }
