63d31146faa5d5bd506f9aaca48e81b838bde681
Quandrax <128057564+Quandrax@users.noreply.github.com>
2025-09-17 18:09:45 +0200

    Added starship

--- home/modules/ghostty.nix
+++ home/modules/ghostty.nix
@@ -1,11 +1,19 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   programs.ghostty = {
     enable = true;
+    enableBashIntegration = true;
     settings = {
       theme = "0x96f";
       background-opacity = 0.8;
+      font-size = 11;
     };
   };
+
+  programs.starship = {
+    enable = true;
+    enableBashIntegration = true;
+  };
 }
+
