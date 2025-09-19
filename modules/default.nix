23e7bbdcf5b3fd17f52f0da82898984b9ddd558a
Quandrax <128057564+Quandrax@users.noreply.github.com>
2025-09-19 12:30:56 +0200

    Fixed headset mic

--- modules/default.nix
+++ modules/default.nix
@@ -21,6 +21,11 @@
       efiSysMountPoint = "/boot/efi";
     };
   };
+  
+  hardware.firmware = [ ( pkgs.writeTextDir "/lib/firmware/hda-jack-retask.fw" ( builtins.readFile ./hda-jack-retask.fw ) ) ];
+  boot.extraModprobeConfig = ''
+  options snd-hda-intel patch=hda-jack-retask.fw
+'';
 
   networking.hostName = "blyat";
   networking.networkmanager.enable = true;  
