d731687bb693af371f4774207247d9e08b0fcafc
Quandrax <128057564+Quandrax@users.noreply.github.com>
2026-06-19 01:54:47 +0200

    updated flake

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -8,6 +8,7 @@
   ++ (lib.filesystem.listFilesRecursive ../../modules);
 
   networking.hostName = "Blyat";
+  hardware.enableRedistributableFirmware = true;
 
   # services.libinput.enable = true;
   # services.printing.enable = true;
