f2a9b0ff22b233588786689acbbd5d7764e1b377
Quandrax <128057564+Quandrax@users.noreply.github.com>
2026-07-03 20:44:29 +0200

    nvm i want it back

--- home/modules/editor.nix
+++ home/modules/editor.nix
@@ -7,7 +7,7 @@
     settings = {
       theme = "monokai_pro";
       #theme = "adwaita-dark";
-      #theme = "ayu_mirage"
+      #theme = "ayu_mirage";
       #theme = "base16_transparent";
       editor.lsp = {
         display-inlay-hints = true;
@@ -30,6 +30,48 @@
       pkgs.tombi
     ];
 
+    defaultEditor = false;
+  };
+
+  programs.zed-editor = {
+    enable = true;
     defaultEditor = true;
+
+    extensions = [
+      "monokai-vibrant-amped"
+      "material-icon-theme"
+
+      "toml"
+      "tombi"
+
+      "zig"
+
+      "slang"
+      "glsl"
+    ];
+
+    userSettings = {
+      helix_mode = true;
+      diagnostics.inline.enabled = true;
+      inlay_hints.enabled = true;
+      disable_ai = true;
+      telemetry = {
+        diagnostics = false;
+        metrics = false;
+      };
+
+      theme = {
+        mode = "dark";
+        light = "Ayu Light";
+        dark = "Monokai Vibrant Amped";
+      };
+      project_panel = {
+        dock = "left";
+      };
+    };
+
+    mutableUserKeymaps = false;
+    mutableUserSettings = false;
+    mutableUserTasks = false;
   };
 }
